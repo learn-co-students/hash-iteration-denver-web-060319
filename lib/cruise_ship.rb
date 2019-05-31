6c8ecdf65b0b273ab710949d07f2f32e7d738a2a
foggybrains <dustin.keyes@gmail.com>
2019-05-30 19:38:27 -0600

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,27 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+    winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+ winner
+end
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
 end
